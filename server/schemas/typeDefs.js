665035671dd4c1638837aa727180d02a28a1f963
mac Ray <rztechdude@yahoo.com>
2022-05-27 16:11:37 -0400

    initial server setup and query test

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,17 @@
+
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+}
+type Query {
+    thoughts(username: String): [Thought]
+}
+`;
+
+module.exports = typeDefs;

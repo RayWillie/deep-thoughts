77b0f048f803026581b06459220fab5eaeebec63
mac Ray <rztechdude@yahoo.com>
2022-05-28 16:19:24 -0400

    feature resolvers and typeDefs complete

--- server/server.js
+++ server/server.js
@@ -5,27 +5,28 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-const server  = new ApolloServer({
+const server = new ApolloServer({
   typeDefs,
   resolvers
 });
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-const startApolloServer = async (tyoeDefs, resolvers) => {
-  await server.start();
  
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
   server.applyMiddleware({ app });
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    //log where to test GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
-}
-
-startApolloServer(typeDefs, resolvers);
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
